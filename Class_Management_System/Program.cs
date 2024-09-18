5fe660bf350e77d399589cd9df16955c95d18f4d
Chamara17-cc <149788803+Chamara17-cc@users.noreply.github.com>
2024-09-19 02:08:17 +0530

    program.cs

--- Class_Management_System/Program.cs
+++ Class_Management_System/Program.cs
@@ -8,31 +8,37 @@ using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 
+// Configure CORS policies
+builder.Services.AddCors(options => {
+    options.AddPolicy("ReactJSDomain",
+        policy => policy.WithOrigins("*") // For development, allow any origin; for production, restrict
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+    );
+});
+
+// Alternative CORS policy (you should use only one CORS policy in production)
+builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
+// Add DbContext configuration
+builder.Services.AddDbContext<DataContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DBCS"))
+);
+
+// Add controllers
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Build the app
 var app = builder.Build();
-builder.Services.AddCors(options => {
-    options.AddPolicy("ReactJSDomain",
-        policy => policy.WithOrigins("*")
-        .AllowAnyHeader()
-        .AllowAnyMethod()
-        .AllowAnyOrigin()
-        );
-});
-builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
-{
-    build.WithOrigins(" *").AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin();
-}));
-builder.Services.AddControllers();
-
-// Configure DbContext
-builder.Services.AddDbContext<DataContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBCS")));
-
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -40,6 +46,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// Use the CORS middleware
+app.UseCors("ReactJSDomain"); // Apply the selected CORS policy
 
 app.UseHttpsRedirection();
 
